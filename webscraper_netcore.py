f2a18234d61abf35e3bbf178fe4c187f42168fa0
Diegobr21 <jdiegobrana@gmail.com>
2020-09-26 21:23:56 -0500

    first commit

---
+++ webscraper_netcore.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome("C:\Program Files\chromedriver")
+
+#BestBuy
+laptops = []
+
+#driver.get("https://www.bestbuy.com.mx/c/laptops/c41")
+driver.get("https://www.bestbuy.com.mx/c/laptops/c41")
+
+content = driver.page_source
+soup = BeautifulSoup(content, features="html.parser")
+for a in soup.findAll('div', attrs={'class':'product-line-item-line'}):
+    name=a.find('div', attrs={'class':'product-title'})
+    laptops.append(name.text)
+
+df = pd.DataFrame({'Laptop Name':laptops}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
